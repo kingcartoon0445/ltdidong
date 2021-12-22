9aafc3ae714db47855ca212fbf64c39cdae0db40
kingcartoon0445 <79097005+kingcartoon0445@users.noreply.github.com>
2021-12-22 21:25:35 +0700

    haizzzz

--- user_flutter/lib/baiviet/baiviet_ds.dart
+++ user_flutter/lib/baiviet/baiviet_ds.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:user_flutter/colorplush.dart';
+import 'chuoibaiviet.dart';
+import 'ListBaiviet.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -20,37 +22,20 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
+      body:  Column(
           children: [
             Text(
               'Bài viết nổi bật',
               style: TextStyle(),
             ),
-             bv_decu(),
-             bv_chuoidecu(),
+              bv_decu(),
+             chuoidecu(),
+             Lst_baiviet(),
           ],
         ),
-      ),
     );
   }
 }
-
-class bv_chuoidecu extends StatelessWidget {
-  const bv_chuoidecu({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> dsdecu = [
-      "Tất cả",
-      "Nhiều lượt xem nhất",
-      "Nhiều like nhất",
-      "Bài viết của bạn"
-    ];
-    return Container();
-    }
-}
-
 class bv_decu extends StatefulWidget {
   const bv_decu({Key? key}) : super(key: key);
 
@@ -63,7 +48,7 @@ class _bv_decuState extends State<bv_decu> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      height: 200,
+      height: 150,
       child: Column(
         children: [
           Expanded(
@@ -73,10 +58,10 @@ class _bv_decuState extends State<bv_decu> {
               controller: PageController(viewportFraction: 0.8, initialPage: 0),
               itemCount: 3, //đếm ảnh
               itemBuilder: (context, index) => Container(
-                margin: EdgeInsets.only(right: 30),
-                width: double.infinity,
+                margin: EdgeInsets.only(right: 14),
+                width: 356,
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(23),
                     image: DecorationImage(
                       image: AssetImage("assets/imgs/baiviets/test.jpg"),
                       fit: BoxFit.cover,
@@ -86,7 +71,7 @@ class _bv_decuState extends State<bv_decu> {
                   children: [
                     Container(
                       width: double.infinity,
-                      height: 60,
+                      height: 51,
                       padding:
                           EdgeInsets.symmetric(horizontal: 23, vertical: 5),
                       decoration: BoxDecoration(
@@ -95,14 +80,15 @@ class _bv_decuState extends State<bv_decu> {
                             bottomRight: Radius.circular(23)),
                         color: kCardInfoBG.withOpacity(0.5),
                       ),
-                      child: Column(
+                      child: Column(  
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Text(
                             "Vẽ đẹp vịnh hạ long",
                             style: TextStyle(
                                 color: Colors.white,
-                                fontSize: 20,
+                                fontSize: 15,
                                 fontWeight: FontWeight.w700),
                           ),
                           Spacer(),
@@ -111,15 +97,16 @@ class _bv_decuState extends State<bv_decu> {
                               Icon(
                                 Icons.place_outlined,
                                 color: Colors.white,
+                                size: 15,
                               ),
                               Text(
                                 "Hạ Long, Quảng Ninh",
-                                style: TextStyle(color: Colors.white),
+                                style: TextStyle(color: Colors.white,fontSize: 10,),
                               ),
                               Spacer(),
                               Text(
                                 'data',
-                                style: TextStyle(color: Colors.white),
+                                style: TextStyle(color: Colors.white,fontSize: 10,),
                               ),
                             ],
                           )
@@ -134,5 +121,41 @@ class _bv_decuState extends State<bv_decu> {
         ],
       ),
     );
+  }
+}
+
+class gt_baiviet extends StatelessWidget {
+  const gt_baiviet({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Container(
+      margin: EdgeInsets.all(20),
+       height: 68,
+      child:Row(
+        children: [
+        Container(
+          alignment: Alignment.topLeft,
+          padding: EdgeInsets.all(20),
+                    width: 144,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(23), 
+             image: DecorationImage(
+                      image: AssetImage("assets/imgs/baiviets/test.jpg"),
+                      fit: BoxFit.cover,),
+          ),
+        ),
+        Expanded(child:  Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+          TextButton(onPressed: (){}, child: Text("Vẻ đẹp Vịnh Hạ Long",style: TextStyle(color: Colors.black,fontWeight:FontWeight.bold,fontSize: 15),)), //#5
+          ElevatedButton.icon(onPressed: (){},style:  ElevatedButton.styleFrom(primary: Colors.white,elevation: 0), icon: Icon(Icons.location_on_outlined,color: Color(0xFF4C56CE),), label: Text("Hạ Long, Quảng Ninh",style: TextStyle(fontSize: 12,color: Color(0xFF828282)),)),
+        
+        ],)),
+      ],
+    ),
+  );
   }
 }
