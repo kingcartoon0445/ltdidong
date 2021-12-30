764a4c88d17b195b245c2bf0166622ee771ffdd6
kingcartoon0445 <79097005+kingcartoon0445@users.noreply.github.com>
2021-12-30 11:51:10 +0700

    thanh duy
    Thay đổ và thêm trang thông tin

--- user_flutter/lib/background.dart
+++ user_flutter/lib/background.dart
@@ -3,10 +3,13 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:user_flutter/baiviet/baiviet_chitiet.dart';
 import 'package:user_flutter/baiviet/baiviet_ds.dart';
+import 'package:user_flutter/class_chung.dart';
 import 'package:user_flutter/diadanh/chitiet_diadanh.dart';
 import 'package:user_flutter/diadanh/danhsach_diadanh.dart';
+import 'package:user_flutter/linhtinh/thongthin2.dart';
 import 'package:user_flutter/linhtinh/thongtin.dart';
 import 'Hoang/login/LoginPage.dart';
+import 'colorplush.dart';
 
 class Background extends StatefulWidget {
   @override
@@ -54,9 +57,55 @@ class _BackgroundState extends State<Background> {
         ),
         centerTitle: true,
         actions: [
-          CircleAvatar(
-            child: Text("D"),
-          )
+          PopupMenuButton(
+            shape:RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(23),
+                bottomLeft: Radius.circular(23),
+                bottomRight:  Radius.circular(23))
+              ),
+            child:CircleAvatar(
+                child: Text("D"),
+             ),
+            itemBuilder: (context)=>[
+              PopupMenuItem(
+                
+                  child: Container(
+                    height: 180, width: 180,child: 
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                       nut_Icon(CircleAvatar(child: Text("s"),radius: 20,),
+                                Text("daat",style: cabin_B(Colors.black, 15),),
+                                Navigator.push(context,
+                                MaterialPageRoute(builder:(context)=> thongtin2()
+                                  ),
+                                ),
+                              ),
+                        Container(
+                        padding: EdgeInsets.only(right: 40),
+                        child: nut_Icon(
+                          Icon(Icons.settings_outlined,size: 25,),
+                          Text("Cài đặt",style: cabin_B(Colors.black, 15),),
+                         context
+                          ),),
+                        OutlinedButton(
+                          onPressed: (){},
+                          style: ButtonStyle(
+                            backgroundColor:MaterialStateProperty.all<Color>(Color(0xFF7D82BC)),
+                            fixedSize: MaterialStateProperty.all(Size(150, 50)) ,
+                            shape:MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(23.0))),
+                          ),
+                          child: Row(
+                            children: [
+                          Icon(Icons.logout_outlined,size: 25,color: Colors.white,),
+                          Text("Đăng xuất",style: cabin_B(Colors.white, 15),)
+                        ],))
+                        ],),
+                       )),
+                    ],
+          ),
         ],
       ),
       bottomNavigationBar: CurvedNavigationBar(
--- user_flutter/lib/linhtinh/thongtin.dart
+++ user_flutter/lib/linhtinh/thongtin.dart
@@ -1,44 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:user_flutter/baiviet/ListBaiviet.dart';
+import 'package:user_flutter/class_chung.dart';
 import 'package:user_flutter/colorplush.dart';
+
 class thongtin extends StatefulWidget {
-  const thongtin({ Key? key }) : super(key: key);
+  const thongtin({Key? key}) : super(key: key);
 
   @override
   _thongtinState createState() => _thongtinState();
 }
 
 class _thongtinState extends State<thongtin> {
+  List<String> a = [];
   @override
+  var _visible = true;
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
         children: [
-          //AVT vs Tên
-          Container(
-            decoration: BoxDecoration(
-              color: Color(0xFF7D82BC),
-              borderRadius: BorderRadius.only(bottomRight: Radius.circular(40),bottomLeft: Radius.circular(40)), // Set rounded corner radius
+           Container(
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF7D82BC),
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(40),
+                        bottomLeft: Radius.circular(40)),
+                  ),
+                  height: 170,
+                  width: 360,
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              CircleAvatar(radius: 50, child: Text("s")),
+                            ]),
+                        Text(
+                          "Đen vâu",
+                          style: cabin_B(Colors.white, 25),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Thông tin",
+                    style: cabin_B(Colors.black, 20),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.only(left: 40, top: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      //nút đã đến
+                      nut_Icon(
+                          SvgPicture.asset(
+                            'assets/imgs/svg/daden.svg',
+                            width: 20,
+                            height: 20,
+                          ),
+                          Text(
+                            "Địa điểm đã đến",
+                            style: cabin(Color(0xFF4C56CE), 15),
+                          ),
+                          context),
+                      //nút địa chỉ
+                      nut_Icon(
+                          SvgPicture.asset(
+                            'assets/imgs/svg/home.svg',
+                            width: 20,
+                            height: 20,
+                          ),
+                          Text(
+                            "Địa chỉ",
+                            style: cabin(Color(0xFF4C56CE), 15),
+                          ),
+                          context),
+                      //nút số điện thoại
+                      nut_Icon(
+                          Icon(
+                            Icons.phone_android,
+                            size: 20,
+                            color: Colors.black,
+                          ),
+                          Text(
+                            "031289412",
+                            style: cabin(Color(0xFF4C56CE), 15),
+                          ),
+                          context),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Danh sách bài viết đã đăng",
+                    style: cabin_B(Colors.black, 20),
+                  ),
+                ),
+                
+              ],
             ),
-            height: 170,
-            width: 360,
-             
-             child: Padding(padding:EdgeInsets.all(15) ,child: Row(
-               crossAxisAlignment: CrossAxisAlignment.center,
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-               children: [
-                 Column( mainAxisAlignment: MainAxisAlignment.end,  children: [CircleAvatar(radius: 50, child: Text("s")),]),
-                 Column( mainAxisAlignment: MainAxisAlignment.end,  children: [Text("Đen vậu",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),]),
-             ],),
-             ),
-          ),Padding(padding: EdgeInsets.only(top:20,left: 40,bottom:10),child:
-          Text("Thông tin",style: TextStyle(color: Colors.black,fontFamily: 'Cabin_B',fontWeight:FontWeight.bold,fontSize: 20),),),
-          Padding(padding: EdgeInsets.only(left:50,bottom: 10),child: Row(children: [Icon(Icons.map_sharp),Text("Da49 den619",style:cabin(Color(0xFF4C56CE), 15),),] ),), 
-           Padding(padding: EdgeInsets.only(left:50,bottom: 10),child: Row(children: [Icon(Icons.map_sharp),Text("Địa chỉ",style:cabin(Color(0xFF4C56CE), 15),),] ),), 
-            Padding(padding: EdgeInsets.only(left:50,bottom: 10),child: Row(children: [Icon(Icons.map_sharp),Text("03124123123",style:cabin(Color(0xFF4C56CE), 15),),] ),), 
-        ],
-      ),
+          ),
+      ]),
     );
   }
 }
