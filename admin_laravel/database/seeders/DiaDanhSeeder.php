279ca5e8251e2e0a9d57444e80617c3624b1c7fa
LeThuannnnn <91464253+LeThuannnnn@users.noreply.github.com>
2022-01-24 17:23:38 +0700

    sửa chi tiết địa danh

--- admin_laravel/database/seeders/DiaDanhSeeder.php
+++ admin_laravel/database/seeders/DiaDanhSeeder.php
@@ -21,6 +21,7 @@ class DiaDanhSeeder extends Seeder
             'MaMien'=>'1',
             'KinhDo'=>'12345',
             'ViDo'=>'54321',
+            'DiaChi'=>'Vịnh Hạ Long',
             'MoTa'=>'Vịnh Hạ Long là một vịnh nhỏ thuộc phần bờ tây vịnh Bắc Bộ tại khu vực biển Đông Bắc Việt Nam, bao gồm vùng biển đảo của thành phố Hạ Long thuộc tỉnh Quảng Ninh'
       ]);
       DB::table('dia_danhs')->insert([
@@ -29,6 +30,7 @@ class DiaDanhSeeder extends Seeder
           'MaMien'=>'2',
           'KinhDo'=>'12345',
           'ViDo'=>'231231',
+          'DiaChi'=>'Huế',
           'MoTa'=>'Huế là thành phố tỉnh lỵ của tỉnh Thừa Thiên Huế, Việt Nam. Huế từng là kinh đô của Việt Nam thời phong kiến dưới triều Tây Sơn và triều Nguyễn. Hiện nay, thành phố là một trong những trung tâm về văn hóa  du lịch, y tế chuyên sâu, giáo dục đào tạo, khoa học công nghệ của Miền Trung Tây Nguyên và cả nước.'
     ]);
     DB::table('dia_danhs')->insert([
@@ -37,6 +39,7 @@ class DiaDanhSeeder extends Seeder
       'MaMien'=>'3',
       'KinhDo'=>'132131',
       'ViDo'=>'12313121',
+      'DiaChi'=>'Cà Mau',
       'MoTa'=>'Tỉnh Cà Mau mang đặc trưng của khí hậu nhiệt đới gió mùa cận xích đạo, với nền nhiệt độ cao vào loại trung bình trong tất cả các tỉnh Đồng bằng sông Cửu Long. Khí hậu Cà Mau được chia thành 2 mùa là mùa mưa và mùa khô.'
     ]);
     }
